ec4e65a06174c3cc3463ae08e1a629a94d35fe79
Marcell Ciszek Druzynski <40737996+masiucd@users.noreply.github.com>
2026-01-18 18:28:50 +0100

    feat: update index route to display product details and remove unused user logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,9 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-import { eq } from "drizzle-orm";
-import { Heading } from "@/components/typography";
-import { Button } from "@/components/ui/button";
-import { db } from "@/db/connect";
-import { product, productImage, user } from "@/db/schema";
+import {createFileRoute} from "@tanstack/react-router"
+import {createServerFn} from "@tanstack/react-start"
+import {eq} from "drizzle-orm"
+import {Heading} from "@/components/typography"
+import {db} from "@/db/connect"
+import {product, productImage} from "@/db/schema"
 
 async function readUsers() {
 	const xs = await db
@@ -15,35 +14,40 @@ async function readUsers() {
 			image: productImage.url,
 		})
 		.from(product)
-		.innerJoin(productImage, eq(productImage.productId, product.id));
-	return xs;
+		.leftJoin(productImage, eq(productImage.productId, product.id))
+	return xs
 }
 
-const getUsers = createServerFn({ method: "GET" }).handler(async () => {
-	return await readUsers();
-});
+const getUsers = createServerFn({method: "GET"}).handler(async () => {
+	return await readUsers()
+})
 
 export const Route = createFileRoute("/")({
 	component: App,
 	loader: async () => {
-		return await getUsers();
+		return await getUsers()
 	},
-});
+})
 
 function App() {
-	const users = Route.useLoaderData();
-
+	const data = Route.useLoaderData()
+	console.log({data})
 	return (
 		<div>
 			<Heading tag="h1">Sick fits</Heading>
-			<Button>Click me</Button>
+
 			<ul>
-				{users.map((user) => (
-					<li key={user.email}>
-						{user.username} - {user.email}
+				{data.map(product => (
+					<li key={product.name} className="my-4">
+						<Heading tag="h2">{product.name}</Heading>
+						<p>{product.description}</p>
+						<p>${product.price}</p>
+						{product.image && (
+							<img src={product.image} alt={product.name} className="w-48 h-48 object-cover" />
+						)}
 					</li>
 				))}
 			</ul>
 		</div>
-	);
+	)
 }
