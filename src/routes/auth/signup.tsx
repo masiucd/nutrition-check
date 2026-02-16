08636e6903b50617676425760ce0fcbfacf5de73
Marcell Ciszek Druzynski <40737996+masiucd@users.noreply.github.com>
2026-02-16 13:35:24 +0700

    Redesign signup form layout and improve validation logic

--- src/routes/auth/signup.tsx
+++ src/routes/auth/signup.tsx
@@ -1,9 +1,10 @@
 import {type AnyFieldApi, useForm} from "@tanstack/react-form"
-import {createFileRoute, replaceEqualDeep} from "@tanstack/react-router"
+import {createFileRoute} from "@tanstack/react-router"
 import {PageWrapper} from "@/components/page_wrapper"
+import {Heading, Text} from "@/components/typography"
 import {Button} from "@/components/ui/button"
-import {Label} from "@/components/ui/label"
 import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
 
 export const Route = createFileRoute("/auth/signup")({
 	component: RouteComponent,
@@ -24,128 +25,158 @@ function RouteComponent() {
 	})
 	return (
 		<PageWrapper>
-			<form
-				className="md:max-w-5xl"
-				onSubmit={e => {
-					e.preventDefault()
-					e.stopPropagation()
-					form.handleSubmit()
-				}}
-			>
-				<fieldset className="flex flex-col gap-3 rounded-md border-2 border-foreground p-2">
-					<legend>Sign up</legend>
-					<div className="flex gap-2">
-						<form.Field
-							name="username"
-							validators={{
-								onBlur: ({value}) =>
-									!value || value.length < 3
-										? "Username is required"
-										: value.length > 32
-											? "Username is too long"
-											: null,
-								onBlurAsyncDebounceMs: 200,
-								onBlurAsync: async ({value}) => {
-									await new Promise(resolve => setTimeout(resolve, 1000))
-									return value.includes("error") && 'No "error" allowed in first name'
-								},
-							}}
-							children={field => (
-								<div className="flex flex-1 flex-col gap-2">
-									<Label htmlFor={field.name}>Username</Label>
-									<Input
-										id={field.name}
-										type="text"
-										name={field.name}
-										value={field.state.value}
-										onBlur={field.handleBlur}
-										onChange={e => field.handleChange(e.target.value)}
-									/>
-									<FieldInfo field={field} />
-								</div>
-							)}
-						/>
-						<form.Field
-							name="email"
-							validators={{
-								onBlur: ({value}) =>
-									!value
-										? "Email is required"
-										: !value.includes("@")
-											? "Invalid email"
-											: !value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
-												? "Invalid email"
+			<div className="mx-auto flex flex-1 flex-col items-center justify-center border-2 p-5 md:max-w-4xl">
+				<div className="flex flex-col items-center justify-center p-5">
+					<Heading tag="h1">Signing up</Heading>
+					<Text tag="lead">Lets create an account for Sick Fits</Text>
+				</div>
+				<form
+					className="w-full flex-1"
+					onSubmit={e => {
+						e.preventDefault()
+						e.stopPropagation()
+						form.handleSubmit()
+					}}
+				>
+					<fieldset className="flex flex-col gap-3 rounded-md border-2 border-foreground p-2">
+						<legend>Sign up</legend>
+						<div className="flex gap-2">
+							<form.Field
+								name="username"
+								validators={{
+									onBlur: ({value}) =>
+										!value || value.length < 3
+											? "Username is required"
+											: value.length > 32
+												? "Username is too long"
 												: null,
-								onBlurAsyncDebounceMs: 200,
-							}}
-							children={field => (
-								<div className="flex flex-1 flex-col gap-2">
-									<Label htmlFor={field.name}>Email</Label>
-									<Input
-										id={field.name}
-										type="text"
-										name={field.name}
-										value={field.state.value}
-										onBlur={field.handleBlur}
-										onChange={e => field.handleChange(e.target.value)}
-									/>
-									<FieldInfo field={field} />
-								</div>
+									onBlurAsyncDebounceMs: 200,
+									onBlurAsync: async ({value}) => {
+										await new Promise(resolve => setTimeout(resolve, 1000))
+										return value.includes("error") && 'No "error" allowed in first name'
+									},
+								}}
+								children={field => (
+									<div className="flex flex-1 flex-col gap-2">
+										<Label htmlFor={field.name}>Username</Label>
+										<Input
+											id={field.name}
+											type="text"
+											name={field.name}
+											value={field.state.value}
+											onBlur={field.handleBlur}
+											onChange={e => field.handleChange(e.target.value)}
+										/>
+										<FieldInfo field={field} />
+									</div>
+								)}
+							/>
+							<form.Field
+								name="email"
+								validators={{
+									onBlur: ({value}) =>
+										!value
+											? "Email is required"
+											: !value.includes("@")
+												? "Invalid email"
+												: !value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+													? "Invalid email"
+													: null,
+									onBlurAsyncDebounceMs: 200,
+								}}
+								children={field => (
+									<div className="flex flex-1 flex-col gap-2">
+										<Label htmlFor={field.name}>Email</Label>
+										<Input
+											id={field.name}
+											type="text"
+											name={field.name}
+											value={field.state.value}
+											onBlur={field.handleBlur}
+											onChange={e => field.handleChange(e.target.value)}
+										/>
+										<FieldInfo field={field} />
+									</div>
+								)}
+							/>
+						</div>
+						<div className="flex gap-2">
+							<form.Field
+								name="password"
+								validators={{
+									onBlur: ({value}) =>
+										!value
+											? "Password is required"
+											: !value.includes("!@#$%^&*()_")
+												? "Password must contain at least one special character"
+												: value.length < 3
+													? "Password must be at least 3 characters long"
+													: null,
+									onBlurAsyncDebounceMs: 500,
+								}}
+								children={field => (
+									<div className="flex flex-1 flex-col gap-2">
+										<Label htmlFor={field.name}>Password</Label>
+										<Input
+											id={field.name}
+											type="password"
+											min={3}
+											name={field.name}
+											value={field.state.value}
+											onBlur={field.handleBlur}
+											onChange={e => field.handleChange(e.target.value)}
+										/>
+										<FieldInfo field={field} />
+									</div>
+								)}
+							/>
+							<form.Field
+								name="repeatPassword"
+								validators={{
+									onBlur: ({value, ...apa}) => {
+										// console.log("Spa", apa.fieldApi.form.state.values.password)
+										// console.log("Spa", apa.fieldApi.form.state.errors)
+										return !value
+											? "Repeat password needs to match the password"
+											: apa.fieldApi.form.state.values.password !== value
+												? "Passwords do not match"
+												: !value.includes("!@#$%^&*()_")
+													? "Password must contain special characters"
+													: null
+									},
+								}}
+								children={field => (
+									<div className="flex flex-1 flex-col gap-2">
+										<Label htmlFor={field.name}>Repeat Password</Label>
+										<Input
+											id={field.name}
+											type="password"
+											name={field.name}
+											value={field.state.value}
+											onBlur={field.handleBlur}
+											onChange={e => field.handleChange(e.target.value)}
+										/>
+										<FieldInfo field={field} />
+									</div>
+								)}
+							/>
+						</div>
+						<form.Subscribe
+							selector={state => [state.canSubmit, state.isSubmitting]}
+							children={([canSubmit, isSubmitting]) => (
+								<Button type="submit" disabled={!canSubmit}>
+									{isSubmitting ? "Creating Account..." : "Create New Account"}
+								</Button>
 							)}
 						/>
-					</div>
-					<div className="flex gap-2">
-						<form.Field
-							name="password"
-							children={field => (
-								<div className="flex flex-1 flex-col gap-2">
-									<Label htmlFor={field.name}>Password</Label>
-									<Input
-										id={field.name}
-										type="password"
-										name={field.name}
-										value={field.state.value}
-										onBlur={field.handleBlur}
-										onChange={e => field.handleChange(e.target.value)}
-									/>
-									<FieldInfo field={field} />
-								</div>
-							)}
-						/>
-						<form.Field
-							name="repeatPassword"
-							children={field => (
-								<div className="flex flex-1 flex-col gap-2">
-									<Label htmlFor={field.name}>Repeat Password</Label>
-									<Input
-										id={field.name}
-										type="password"
-										name={field.name}
-										value={field.state.value}
-										onBlur={field.handleBlur}
-										onChange={e => field.handleChange(e.target.value)}
-									/>
-									<FieldInfo field={field} />
-								</div>
-							)}
-						/>
-					</div>
-					<form.Subscribe
-						selector={state => [state.canSubmit, state.isSubmitting]}
-						children={([canSubmit, isSubmitting]) => (
-							<Button type="submit" disabled={!canSubmit}>
-								{isSubmitting ? "signingup..." : "Signup"}
-							</Button>
-						)}
-					/>
-				</fieldset>
-			</form>
+					</fieldset>
+				</form>
+			</div>
 		</PageWrapper>
 	)
 }
 
 function FieldInfo({field}: {field: AnyFieldApi}) {
-	console.log("Valid", field.state.meta.errors)
 	return (
 		<>
 			{field.state.meta.isTouched && !field.state.meta.isValid ? (
