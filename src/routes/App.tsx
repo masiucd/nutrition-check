821da0d5f1eb2fd93399681f1e98990edbd096f0
Marcell Ciszek Druzynski <40737996+masiucd@users.noreply.github.com>
2026-04-02 19:57:48 +0200

    new project

--- src/routes/App.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/App')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/App"!</div>
-}
