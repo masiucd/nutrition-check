7fe6d1f9ddf96e840e7424b44e0895c3ba5de9f3
Marcell Ciszek Druzynski <40737996+masiucd@users.noreply.github.com>
2026-04-03 11:39:49 +0200

    Load and display test_data from database on home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,26 @@
 import {createFileRoute} from "@tanstack/react-router"
+import {createServerFn} from "@tanstack/react-start"
 import {Button} from "@/components/ui/button"
+import {sql} from "@/db"
 
-export const Route = createFileRoute("/")({component: App})
+const fn = createServerFn({method: "GET"}).handler(async () => {
+	const r = await sql`
+      SELECT * FROM test_data
+    `
+	return r
+})
+
+export const Route = createFileRoute("/")({
+	component: App,
+	loader: async () => {
+		const xs = await fn()
+		return xs
+	},
+})
 
 function App() {
+	const _data = Route.useLoaderData()
+	console.log(_data)
 	return (
 		<div className="flex min-h-svh p-6">
 			<div className="flex min-w-0 max-w-md flex-col gap-4 text-sm leading-loose">
@@ -13,6 +30,11 @@ function App() {
 					<p>We&apos;ve already added the button component for you.</p>
 					<Button className="mt-2">Button</Button>
 				</div>
+				<ul>
+					{_data.map(x => (
+						<li key={x.id}>{x.text}</li>
+					))}
+				</ul>
 			</div>
 		</div>
 	)
